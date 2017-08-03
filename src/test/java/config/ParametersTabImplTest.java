8ec4a35e36ea418be0da738a2e18d38e6761f556
Miles Efron <mefron@gmail.com>
2017-08-03 14:32:56 -0600

    added junit test for Parameters

---
+++ src/test/java/config/ParametersTabImplTest.java
@@ -0,0 +1,24 @@
+package config;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ParametersTabImplTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
